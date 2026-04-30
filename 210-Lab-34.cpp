0e2d75b93ae74a21c34eb4cc265e06f793d2e22a
JoaquinF <joaquinpfelix@gmail.com>
2026-04-30 16:50:00 -0700

    Step 2 Complete

--- 210-Lab-34.cpp
+++ 210-Lab-34.cpp
@@ -4,17 +4,21 @@
 
 int main()
 {
-    Graph g(7);
+    Graph g(9);
 
-    g.addEdge(0,1,12);
-    g.addEdge(0,2,8);
-    g.addEdge(0,3,21);
-    g.addEdge(2,3,6);
-    g.addEdge(2,6,2);
-    g.addEdge(2,4,4);
-    g.addEdge(2,5,5);
-    g.addEdge(4,5,6);
-    g.addEdge(5,6,6);
+    g.addEdge(0,1,8);
+    g.addEdge(0,2,21);
+    g.addEdge(1,2,6);
+    g.addEdge(1,3,5);
+    g.addEdge(1,4,4);
+    g.addEdge(2,7,11);
+    g.addEdge(2,8,8);
+    g.addEdge(3,4,9);
+    g.addEdge(5,6,10);
+    g.addEdge(5,7,15);
+    g.addEdge(5,8,5);
+    g.addEdge(6,7,3);
+    g.addEdge(6,8,7);
 
     g.printGraph();
 
